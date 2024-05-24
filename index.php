65f7630476e53c0188846cd3770acbdbcdad4115
adrianoposumah <113793119+adrianoposumah@users.noreply.github.com>
2024-05-24 22:38:15 +0800

    modified index.php

--- index.php
+++ index.php
@@ -1,65 +1,132 @@
 <?php include 'navbar.php'; ?>
-    <section>
-        <div class="carousel-container">
-            <div class="mySlides fade">
-            <div class="film-banner">
-                <img src="./image/movie-banner/Money-Heist-banner.jpg" style="width:100%">
-            </div>
-                <div class="film-information">
-                    <div class="film-title">Money Heist</div>
-                    <div class="film-prop">
-                        <div class="film-year">2022 | </div>
-                        <div class="film-genre">Action · Crime · </div>
-                        <div class="film-type">TV Series</div>
-                    </div>
-                    <div class="film-synopsis">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-                    </div>
-                </div>
-            </div>
-            <div class="mySlides fade">
-           
-                <img src="./image/movie-banner/dune2-banner.jpg" style="width:100%">
-                <div class="film-information">
-                    <div class="film-title">Money Heist</div>
-                    <div class="film-prop">
-                        <div class="film-year">2022 | </div>
-                        <div class="film-genre">Action · Crime · </div>
-                        <div class="film-type">TV Series</div>
-                    </div>
-                    <div class="film-synopsis">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-                    </div>
-                </div>
-            </div>
-            <div class="mySlides fade">
-                <img src="./image/movie-banner/Money-Heist-banner.jpg" style="width:100%">
-               <div class="film-information">
-                    <div class="film-title">Money Heist</div>
-                    <div class="film-prop">
-                        <div class="film-year">2022 | </div>
-                        <div class="film-genre">Action · Crime · </div>
-                        <div class="film-type">TV Series</div>
-                    </div>
-                    <div class="film-synopsis">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
-                    </div>
-                </div>
-            </div>
+<section>
+  <div class="carousel-container">
+    <div class="mySlides fade">
+      <div class="film-banner">
+        <img src="./image/movie-banner/Money-Heist-banner.jpg" style="width: 100%" />
+      </div>
+      <div class="film-information">
+        <div class="film-title">Money Heist</div>
+        <div class="film-prop">
+          <div class="film-year">2022 |</div>
+          <div class="film-genre">Action · Crime ·</div>
+          <div class="film-type">TV Series</div>
         </div>
-       
-    </section>
-    <div class="search-field">
-       
-          <form>
-            <input type="text" name="search" id="srch" placeholder="Search" />
-            <button type="submit"><i class="fa fa-search"></i></button>
-          </form>
-        
+        <div class="film-synopsis">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+      </div>
     </div>
-    <section>
-        <div class="featured">
-            <h1>Featured</h1>
+    <div class="mySlides fade">
+      <img src="./image/movie-banner/dune2-banner.jpg" style="width: 100%" />
+      <div class="film-information">
+        <div class="film-title">Money Heist</div>
+        <div class="film-prop">
+          <div class="film-year">2022 |</div>
+          <div class="film-genre">Action · Crime ·</div>
+          <div class="film-type">TV Series</div>
         </div>
-    </section>
+        <div class="film-synopsis">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+      </div>
+    </div>
+    <div class="mySlides fade">
+      <img src="./image/movie-banner/Money-Heist-banner.jpg" style="width: 100%" />
+      <div class="film-information">
+        <div class="film-title">Money Heist</div>
+        <div class="film-prop">
+          <div class="film-year">2022 |</div>
+          <div class="film-genre">Action · Crime ·</div>
+          <div class="film-type">TV Series</div>
+        </div>
+        <div class="film-synopsis">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</div>
+      </div>
+    </div>
+  </div>
+</section>
+<div class="search-field">
+  <form>
+    <input type="text" name="search" id="srch" placeholder="Search" />
+    <button type="submit"><i class="fa fa-search"></i></button>
+  </form>
+</div>
+<section>
+  <div class="featured">
+    <h1>FEATURED</h1>
+    <div class="movie-container">
+      <div class="movie-card">
+        <img src="image/movie-poster/dune-poster.jpg" alt="[Movie 1 Poster]" />
+        <h3>Movie Title 1</h3>
+        <p>Year 1</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/dune2-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>Movie Title 1</h3>
+        <p>Year 1</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/fallout-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>Movie Title 1</h3>
+        <p>Year 1</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/ghostbuster-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>Movie Title 1</h3>
+        <p>Year 1</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/godzillaminone-poster.jpg" alt="[Movie 1 Poster]" />
+        <h3>[Movie Title 1]</h3>
+        <p>[Year 1]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/gxk-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/gxk-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/invincible-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/kunfupanda4-poster.jpg" alt="[Movie 1 Poster]" />
+        <h3>[Movie Title 1]</h3>
+        <p>[Year 1]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/loki-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/madameweb-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/The Ministry of Ungentlemanly Warfare-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/thefallguy-poster.jpg" alt="[Movie 1 Poster]" />
+        <h3>[Movie Title 1]</h3>
+        <p>[Year 1]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/twdtowl-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+      <div class="movie-card">
+        <img src="image/movie-poster/ghostbuster-poster.jpg" alt="[Movie 2 Poster]" />
+        <h3>[Movie Title 2]</h3>
+        <p>[Year 2]</p>
+      </div>
+    </div>
+  </div>
+</section>
 <?php include 'footer.php'; ?>
