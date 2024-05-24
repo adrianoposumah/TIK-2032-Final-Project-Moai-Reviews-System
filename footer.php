da4e4e858bae57601acd21b9397ed2359f5b46a8
adrianoposumah <113793119+adrianoposumah@users.noreply.github.com>
2024-05-24 18:26:25 +0800

    config db categories

---
+++ footer.php
@@ -0,0 +1,31 @@
+    <footer>
+      <div class="footer-content">
+        <div class="logo">
+          <img src="./image/auth-picture/logo.png" alt="" />
+        </div>
+        <div class="categories">
+          <h4>Categories</h4>
+          <div class="columns">         
+            <ul>
+              <?php foreach($categories as $category) : ?>
+                <li><a href="#"><?= $category['name'] ?></a></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom">
+        <div class="bottom-area">
+          <div class="copyright">
+            <p>2024 Copyright All Rights Reserved</p>
+          </div>
+          <div class="about">
+            <p>About Us</p>
+            <i class="fa-solid fa-chevron-up"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+  <script src="/script/script.js"></script>
+</html>
--- navbar.php
+++ navbar.php
@@ -1,39 +1,55 @@
+<?php
+require './config/connection.php';
+require './config/functions.php';
+
+$categories = query("SELECT * FROM `genres`");
+?>
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Moai Reviews</title>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Awesome CSS Responsive Navigation menus </title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
 
-  <link rel="stylesheet" href="style/navbar.css">
-</head>
+    <link rel="stylesheet" href="./style/style.css" />
 
-<body>
-  <header>
-    <input type="checkbox" name="" id="chk1">
-    <div class="navbar-logo">
-      <img src="image/logo.svg" alt="Moaireviews logo">
-    </div>
-    <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Categories</a></li>
-      <li><a href="#">Sign up</a></li>
-      <li class="signin"><a href="#">Sign in</a></li>
-    </ul>
-    <div class="menu">
-      <label for="chk1">
-        <i class="fa fa-bars"></i>
-      </label>
-    </div>
-    <div class="search-box">
-      <form>
-        <input type="text" name="search" id="srch" placeholder="Search">
-        <button type="submit"><img src="image/search.png" alt="Search"></button>
-      </form>
-    </div>
-  </header>
-</body>
+    <script src="https://kit.fontawesome.com/bfff52efaa.js" crossorigin="anonymous"></script>
+  </head>
 
-</html>
+  <body>
+    <header>
+      <div class="navbar">
+        <div class="navbar-logo">
+          <img src="./image/logo.svg" alt="Moaireviews logo" />
+        </div>
+        <div class="navbar-right">
+          <div class="navbar-list">
+            <ul>
+              <li><a href="./index.php">Home</a></li>
+              <li><a href="./categories.php">Categories</a></li>
+              <li><a href="./signup.php">Sign up</a></li>
+              <li class="signin"><a href="./login.php">Sign in</a></li>
+            </ul>
+          </div>
+          <div class="menu">
+            <label for="chk1">
+              <i class="fa fa-bars"></i>
+            </label>
+          </div>
+          <div class="search-box">
+            <form>
+              <input type="text" name="search" id="srch" placeholder="Search" />
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
