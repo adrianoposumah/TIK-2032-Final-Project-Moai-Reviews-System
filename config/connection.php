76a5e4b999f4dbfacf8ae9b7e81c644fc8e4f9b0
adrianoposumah <113793119+adrianoposumah@users.noreply.github.com>
2024-06-10 22:20:16 +0800

    .

--- config/connection.php
+++ config/connection.php
@@ -1,5 +1,6 @@
 <?php
 $conn = mysqli_connect("us-cluster-east-01.k8s.cleardb.net", "b0bc1b1d32a293", "eede183c",  "heroku_daaf5f01f17dae7");
+// $conn = mysqli_connect("localhost", "root", "020804apip",  "moaireviewsystem");
 if (mysqli_connect_errno()) {
   echo "Koneksi database gagal : " . mysqli_connect_error();
 }
